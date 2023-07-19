b4eb23947d78f300fdc3ac1d8f3377f7bbf035df
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-07-19 12:06:23 +0000

    Release 0.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * as Vocode from "./api";
 export { VocodeClient } from "./Client";
+export { VocodeEnvironment } from "./environments";
 export { VocodeError, VocodeTimeoutError } from "./errors";
