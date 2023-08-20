2212bb0b0ccb9a0c09eb1ae4eed83ee7ea2ab8d7
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-08-20 01:23:02 +0000

    Release 0.0.9

--- src/api/resources/usage/client/Client.ts
+++ src/api/resources/usage/client/Client.ts
@@ -26,7 +26,7 @@ export class Usage {
                 Authorization: await this._getAuthorizationHeader(),
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@vocode/vocode-api",
-                "X-Fern-SDK-Version": "0.0.8",
+                "X-Fern-SDK-Version": "0.0.9",
             },
             contentType: "application/json",
             timeoutMs: 60000,
